5383303d469248d956ca2970a38827cbbe9e5519
chaudharyhassaan <hassaanchaudhary2007@gmail.com>
2026-05-08 23:16:12 +0500

    Keeping one message

--- Semester Project OOP/Project.cpp
+++ Semester Project OOP/Project.cpp
@@ -4,12 +4,6 @@ int main()
 {
 	cout << "Hi I am testing my Github Integration";
 	cout<<"Hi I am testing commit change from github to VS 2026";
-	cout << "Hi I am testing my Github Integration";
-	cout<<"Hi I am testing commit change from github to VS 2026";
-	cout << "Hi I am testing my Github Integration";
-	cout<<"Hi I am testing commit change from github to VS 2026";
-	cout << "Hi I am testing my Github Integration";
-	cout<<"Hi I am testing commit change from github to VS 2026";
-	cout << "Testing Fetch";
+	
 	return 0;
 }	
